01d91a7d86e2d3f673ab75d5bd984fb2745f405f
miz <didehgur1@naver.com>
2021-03-22 22:30:33 +0900

    210322 first commit

---
+++ src/main/java/miz/springboot/repository/MemoryMemberRepository.java
@@ -0,0 +1,45 @@
+package miz.springboot.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import miz.springboot.domain.Member;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+	private static Map<Long, Member> store = new HashMap<>();
+	private static long sequence = 0L;//동시성 문제 고민
+
+	@Override
+	public Member save(Member member) {
+		member.setId(++sequence);
+		store.put(member.getId(),member);
+		return member;
+	}
+
+	@Override
+	public Optional<Member> findById(Long id) {
+		return Optional.of(store.get(id));
+	}
+
+	@Override
+	public Optional<Member> findByName(String name) {
+		return store.values().stream()
+			.filter(member -> member.getName().equals(name))
+			.findAny();
+	}
+
+	@Override
+	public List<Member> findAll() {
+		return new ArrayList<>(store.values());
+	}
+
+	public void clearStore() {
+		store.clear();
+	}
+}
